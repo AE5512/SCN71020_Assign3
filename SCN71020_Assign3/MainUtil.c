f58a9487d4feae51f5d4c9f16e2a16438c8017e7
AE5512 <aenglish5512@conestogac.on.ca>
2024-10-30 11:12:44 -0400

    Added a unit test

---
+++ SCN71020_Assign3/MainUtil.c
@@ -0,0 +1,6 @@
+#include "MainUtil.h"
+
+char gameFunction(Player1input, Player2input) {
+
+
+}
--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+// Tell compiler to use C
+extern "C" char gameResult(char player1, char player2);
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
@@ -9,8 +12,14 @@ namespace UnitTest1
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(resultWasNotInvalid)
 		{
+			char* player1 = "paper";
+			char* player2 = "rock";
+			char result = gameResult(*player1, *player2);
+			
+			Assert::AreNotEqual("invalid", &result);
+
 		}
 	};
 }
