abb00c108d2d2d02ef80b0586b94289f2a59760c
AE5512 <aenglish5512@conestogac.on.ca>
2024-10-30 11:41:46 -0400

    Added missing closing brackets to statements

--- SCN71020_Assign3/MainUtil.c
+++ SCN71020_Assign3/MainUtil.c
@@ -1,31 +1,33 @@
 #include "MainUtil.h"
 
 char* gameFunction(char* player1input, char* player2input) {
-	if (player1input == player2input) {
+	if (player1input == player2input) { //draw
 		return "Draw";
 	}
-	else if (player1input == "rock"){
+	else if (player1input == "rock") { //player 1 picks rock
 		if (player2input == "scissors") {
 			return "Player1";
 		}
 		else if (player2input == "paper") {
 			return "Player2";
 		}
-	else if (player1input == "paper") {
+	}
+	else if (player1input == "paper") { //player 1 picks paper
 			if (player2input == "rock") {
 				return "Player1";
 			}
 			else if (player2input == "scissors") {
 				return "Player2";
 			}
-	else if (player1input == "scissors") {
+	}
+	else if (player1input == "scissors") { //player 1 picks scissors
 		if (player2input == "paper") {
 			return "Player1";
 		}
 		else if (player2input == "rock") {
 			return "Player2";
 		}
-}
+	}
 	else {
 		return "Invalid";
 	}
